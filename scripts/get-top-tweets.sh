0e3f0240ecdf2c809ae1d9672e8042cb187770a3
@snowman <jimmoffitt@yahoo.com>
2019-01-05 13:21:28 -0700

    Update get-top-tweets.sh

--- scripts/get-top-tweets.sh
+++ scripts/get-top-tweets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Make a Search API request... Client paginates to request all data by default.
 
-ruby ../search/search-app.rb -r "snow has:media has:geo -is:retweet" -s 1d -a "../config/environments_private.yaml" -c "../search/config/settings.yaml"
+ruby search/search-app.rb -r "snow has:media has:geo -is:retweet" -s 1d -a "../config/environments_private.yaml" -c "../search/config/settings.yaml"
 #Pass Tweet IDs to the Engagement API. 
-ruby ../engagement/engagement-app.rb -a "../config/environments_private.yaml" -c "../engagement/config/settings.yaml"
+ruby engagement/engagement-app.rb -a "../config/environments_private.yaml" -c "../engagement/config/settings.yaml"
