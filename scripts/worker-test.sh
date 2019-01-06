b165e8b897c4b91aeb019467a7ded254997a8f4e
@snowman <jimmoffitt@yahoo.com>
2019-01-05 17:01:32 -0700

    Update worker-test.sh

--- scripts/worker-test.sh
+++ scripts/worker-test.sh
@@ -1,6 +1,5 @@
 echo "Worker launched..."
-echo "Calling Ruby test script..."
+echo "Calling Ruby test script.... Working dir: " && pwd
 ruby scripts/ruby-test.rb
-pwd
 echo "Back from test ruby call... Now attempting the real thing at scripts/ruby-test.rb"
 bash scripts/get-top-tweets.sh
